1fb3612cda8b7a4f8d62395d47b81f4b57e4ebbc
Alexander Karpov <alexander.karpoff@gmail.com>
2017-09-11 23:43:29 +0300

    Lecture 60: Dynamically Loading Home View Content

--- docs/project/js/script.js
+++ docs/project/js/script.js
@@ -12,3 +12,30 @@ $(function () {
     });
 
 });
+
+(function (global) {
+    var dc = {};
+
+    var homeHtml = "snippets/home-snippet.html";
+
+    var insertHtml = function (selector, html) {
+        var targetElem = document.querySelector(selector);
+        targetElem.innerHTML = html;
+    };
+
+    var showLoading = function (selector) {
+        var html = "<div class='text-center'><img src='images/ajax-loader.gif'></div>";
+        insertHtml(selector, html);
+    };
+
+    document.addEventListener("DOMContentLoaded", function (event) {
+       showLoading("#main-content");
+       $ajaxUtils.sendGetRequest(homeHtml, function (responseText) {
+           document.querySelector("#main-content").innerHTML = responseText;
+       }, false);
+
+    });
+
+    global.$dc = dc;
+
+})(window);
