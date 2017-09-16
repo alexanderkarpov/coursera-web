1933f13fe74d6e4e7b9eebc8da51e24848cbf40b
Alexander Karpov <alexander.karpoff@gmail.com>
2017-09-16 20:03:20 +0300

    lect. 12: filters

--- angularjs/module2/lecture12-filters/app.js
+++ angularjs/module2/lecture12-filters/app.js
@@ -2,22 +2,41 @@
     'use strict';
 
     angular.module('MsgApp', [])
-        .controller('MsgController', MsgController );
+        .controller('MsgController', MsgController )
+        //STEP 2: register filter factory
+        .filter('loves', LovesFilter)
+        .filter('truth', TruthFilter);
 
-    MsgController.$inject = ['$scope', '$filter'];
-    function MsgController($scope, $filter) {
+    MsgController.$inject = ['$scope', '$filter', 'lovesFilter']; //STEP3: inject filter factory
+    function MsgController($scope, $filter, lovesFilter) {
         $scope.name = "Yaakov";
         $scope.stateOfBeing = "hungry";
         $scope.cookieCost = 0.45;
 
         $scope.sayMessage = function () {
             var msg = "Yaakov likes to eat healthy snacks at night";
-            var output = $filter('uppercase')(msg);
+            // var output = $filter('uppercase')(msg);
+            var output = lovesFilter(msg);
             return output;
         };
 
         $scope.feedYaakov = function () {
             $scope.stateOfBeing = "fed";
+        }
+    }
+
+    //STEP 1: create filter factory
+    function LovesFilter() {
+        return function (input) {
+            input = input || "";
+            return input.replace("likes", "loves");
+        }
+    }
+
+
+    function TruthFilter() {
+        return function (input, target, replace) {
+            return (input || "").replace(target, replace)
         }
     }
 
