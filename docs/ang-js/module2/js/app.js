8e7d234d949cc63bb8a10af3442af7f11dbae797
Alexander Karpov <alexander.karpoff@gmail.com>
2017-09-18 08:30:22 +0300

    assignment 2

--- docs/ang-js/module2/js/app.js
+++ docs/ang-js/module2/js/app.js
@@ -1,3 +1,62 @@
 (function () {
-   'use strict'
-});
+    'use strict';
+
+    angular
+        .module("ShoppingListCheckOffApp", [])
+        .controller("ToBuyListController", ToBuyListController)
+        .controller("AlreadyBoughtListController", AlreadyBoughtListController)
+        .service("ToBuyListService", ToBuyListService)
+        .service("AlreadyBoughtListService", AlreadyBoughtListService);
+
+    ToBuyListController.$inject = ['ToBuyListService'];
+
+    function ToBuyListController(ToBuyListService) {
+        var showList = this;
+
+        showList.items = ToBuyListService.getItems();
+
+        showList.removeItem = function (itemIndex) {
+            ToBuyListService.removeItem(itemIndex);
+        };
+    }
+
+    AlreadyBoughtListController.$inject = ['AlreadyBoughtListService'];
+
+    function AlreadyBoughtListController(AlreadyBoughtListService) {
+        var showList = this;
+
+        showList.items = AlreadyBoughtListService.getItems();
+    }
+
+    function ToBuyListService() {
+        var service = this;
+        var items = [
+            {name: "cookies", quantity: "10 bags"},
+            {name: "tomatoes", quantity: "1 kg"},
+            {name: "potatoes", quantity: "5 kg"},
+            {name: "beef", quantity: "0.7 kg"},
+            {name: "bread", quantity: "1 loaf"}
+        ];
+
+        service.getItems = function () {
+            return items;
+        };
+
+        service.removeItem = function (itemIndex) {
+            items.splice(itemIndex, 1);
+            //TODO: add to AlreadyBoughtListService
+        };
+
+
+    }
+
+    function AlreadyBoughtListService() {
+        var service = this;
+        var items = [];
+
+        service.addItem = function (item) {
+            items.push(item);
+        }
+    }
+
+})();
