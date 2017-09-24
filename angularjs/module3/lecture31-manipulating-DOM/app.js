6008f5dfb82bf2b354a1e3634c8f568620e91dff
Alexander Karpov <alexander.karpoff@gmail.com>
2017-09-24 22:29:12 +0300

    Lecture31: manipulating DOM with the link

--- angularjs/module3/lecture31-manipulating-DOM/app.js
+++ angularjs/module3/lecture31-manipulating-DOM/app.js
@@ -12,10 +12,9 @@
             templateUrl: 'shoppingList.html',
             scope: {
                 items: '<',
-                title: '@',
-                onRemove: '&' //reference binding
+                myTitle: '@title',
+                onRemove: '&'
             },
-            // controller: 'ShoppingListDirectiveController as list',
             controller: ShoppingListDirectiveController,
             controllerAs: 'list',
             bindToController: true,
@@ -25,43 +24,48 @@
         return ddo;
     }
 
-    function ShoppingListDirectiveLink(scope, element, attr, controller) {
+
+    function ShoppingListDirectiveLink(scope, element, attrs, controller) {
         console.log("Link scope is: ", scope);
         console.log("Controller instance is: ", controller);
         console.log("Element is: ", element);
 
-        scope.$watch('list.cookiesInList()', function (newVal, oldVal) {
-            console.log("Old value", oldVal);
-            console.log("New value", newVal);
+        scope.$watch('list.cookiesInList()', function (newValue, oldValue) {
+            console.log("Old value: ", oldValue);
+            console.log("New value: ", newValue);
 
-            if (newVal === true) {
+            if (newValue === true) {
                 displayCookieWarning();
-            } else {
+            }
+            else {
                 removeCookieWarning();
             }
+
         });
 
+        function displayCookieWarning() {
+            // Using Angluar jqLite
+            // var warningElem = element.find("div");
+            // console.log(warningElem);
+            // warningElem.css('display', 'block');
+
+            // If jQuery included before Angluar
+            var warningElem = element.find("div.error");
+            warningElem.slideDown(900);
+        }
+
+
+        function removeCookieWarning() {
+            // Using Angluar jqLite
+            // var warningElem = element.find("div");
+            // warningElem.css('display', 'none');
+
+            // If jQuery included before Angluar
+            var warningElem = element.find("div.error");
+            warningElem.slideUp(900);
+        }
     }
 
-    function displayCookieWarning() {
-        //using angular jqLite
-        // var warningElem = element.find("div");
-        // warningElem.css('display', 'black');
-
-        // var warningElem = element.find("div.error");
-        var warningElem = element.find("#error-message");
-        warningElem.slideDown(900);
-    }
-
-    function removeCookieWarning() {
-        // var warningElem = element.find("div");
-        // warningElem.css('display', 'none');
-        // var warningElem = element.find("div.error");
-        var warningElem = element.find("#error-message");
-
-        warningElem.slideUp(900);
-
-    }
 
     function ShoppingListDirectiveController() {
         var list = this;
@@ -82,29 +86,28 @@
     ShoppingListController.$inject = ['ShoppingListFactory'];
 
     function ShoppingListController(ShoppingListFactory) {
-        var list = this;
+        var viewList = this;
 
         // Use factory to create new shopping list service
         var shoppingList = ShoppingListFactory();
 
-        list.items = shoppingList.getItems();
+        viewList.items = shoppingList.getItems();
         var origTitle = "Shopping List #1";
-        list.title = origTitle + " (" + list.items.length + " items )";
+        viewList.title = origTitle + " (" + viewList.items.length + " items )";
 
-        list.itemName = "";
-        list.itemQuantity = "";
+        viewList.itemName = "";
+        viewList.itemQuantity = "";
 
-        list.addItem = function () {
-            console.log("add", list.itemName);
-            shoppingList.addItem(list.itemName, list.itemQuantity);
-            list.title = origTitle + " (" + list.items.length + " items )";
+        viewList.addItem = function () {
+            shoppingList.addItem(viewList.itemName, viewList.itemQuantity);
+            viewList.title = origTitle + " (" + viewList.items.length + " items )";
         };
 
-        list.removeItem = function (itemIndex) {
+        viewList.removeItem = function (itemIndex) {
             console.log("'this' is: ", this);
             this.lastRemoved = "Last item removed was " + this.items[itemIndex].name;
             shoppingList.removeItem(itemIndex);
-            list.title = origTitle + " (" + list.items.length + " items )";
+            this.title = origTitle + " (" + viewList.items.length + " items )";
         };
     }
 
