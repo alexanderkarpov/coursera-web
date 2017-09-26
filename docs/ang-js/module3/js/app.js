fe7cca5076a4a3d30f9af0530b66bac2f53e5d0b
Alexander Karpov <alexander.karpoff@gmail.com>
2017-09-26 22:50:37 +0300

    assignment 3: smth from lecture 30 were added

--- docs/ang-js/module3/js/app.js
+++ docs/ang-js/module3/js/app.js
@@ -5,12 +5,7 @@
         .controller("NarrowItDownController", NarrowItDownController)
         .service("MenuSearchService", MenuSearchService)
         .directive('foundItems', FoundItemsDirective)
-        .constant('ApiUrl', "https://davids-restaurant.herokuapp.com/menu_items.json")
-
-        .controller('ShoppingListController', ShoppingListController)
-        // .factory('ShoppingListFactory', ShoppingListFactory)
-        .service('ShoppingListService', ShoppingListService)
-        .directive('shoppingList', ShoppingListDirective);
+        .constant('ApiUrl', "https://davids-restaurant.herokuapp.com/menu_items.json");
 
     NarrowItDownController.$inject = ['MenuSearchService'];
     MenuSearchService.$inject = ['$q', '$http', 'ApiUrl'];
@@ -92,97 +87,6 @@
                 method: "GET",
                 url: (ApiUrl)
             });
-        };
-    }
-
-    //----------------------------
-
-    function ShoppingListDirective() {
-        var ddo = {
-            templateUrl: 'shoppingList.html',
-            scope: {
-                items: '<',
-                title: '@',
-                badRemove: '=',
-                onRemove: '&' //reference binding
-            },
-            // controller: 'ShoppingListDirectiveController as list',
-            controller: ShoppingListDirectiveController,
-            controllerAs: 'list',
-            bindToController: true
-        };
-
-        return ddo;
-    }
-
-
-    function ShoppingListDirectiveController() {
-        var list = this;
-
-        list.cookiesInList = function () {
-            for (var i = 0; i < list.items.length; i++) {
-                var name = list.items[i].name;
-                if (name.toLowerCase().indexOf("cookie") !== -1) {
-                    return true;
-                }
-            }
-
-            return false;
-        };
-    }
-
-
-    ShoppingListController.$inject = ['ShoppingListService'];
-
-    function ShoppingListController(ShoppingListService) {
-        var list = this;
-
-        // Use factory to create new shopping list service
-        var shoppingList = ShoppingListService;
-
-        list.items = shoppingList.getItems();
-        var origTitle = "Shopping List #1";
-        list.title = origTitle + " (" + list.items.length + " items )";
-
-        list.itemName = "";
-        list.itemQuantity = "";
-
-        list.addItem = function () {
-            console.log("add", list.itemName);
-            shoppingList.addItem(list.itemName, list.itemQuantity);
-            list.title = origTitle + " (" + list.items.length + " items )";
-        };
-
-        list.removeItem = function (itemIndex) {
-            console.log("'this' is: ", this);
-            this.lastRemoved = "Last item removed was " + this.items[itemIndex].name;
-            shoppingList.removeItem(itemIndex);
-            list.title = origTitle + " (" + list.items.length + " items )";
-        };
-    }
-
-
-// If not specified, maxItems assumed unlimited
-    function ShoppingListService() {
-        var service = this;
-
-        // List of shopping items
-        var items = [];
-
-        service.addItem = function (itemName, quantity) {
-            var item = {
-                name: itemName,
-                quantity: quantity
-            };
-            items.push(item);
-        };
-
-        service.removeItem = function (itemIndex) {
-            items.splice(itemIndex, 1);
-        };
-
-        service.getItems = function () {
-            return items;
         };
     }
 
