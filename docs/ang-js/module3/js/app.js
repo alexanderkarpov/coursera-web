2b00b0f158b79296749087bc4594fa731de160d1
Alexander Karpov <alexander.karpoff@gmail.com>
2017-09-26 23:22:33 +0300

    assignment 3: directives: step 1

--- docs/ang-js/module3/js/app.js
+++ docs/ang-js/module3/js/app.js
@@ -15,7 +15,7 @@
         var ddo = {
             templateUrl: 'foundItems.html',
             scope: {
-                list: '=myList'
+                list: '<myList'
             }
             // controller: NarrowItDownController,
             // controllerAs: 'list',
