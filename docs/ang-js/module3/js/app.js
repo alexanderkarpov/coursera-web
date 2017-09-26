509e6c0b8ffb5368c52ac5b6fae141b210e5dace
Alexander Karpov <alexander.karpoff@gmail.com>
2017-09-26 08:22:07 +0300

    assignment 3

--- docs/ang-js/module3/js/app.js
+++ docs/ang-js/module3/js/app.js
@@ -4,15 +4,35 @@
     angular.module("NarrowItDownApp", [])
         .controller("NarrowItDownController", NarrowItDownController)
         .service("MenuSearchService", MenuSearchService)
+        .directive('foundItems', FoundItemsDirective)
         .constant('ApiUrl', "https://davids-restaurant.herokuapp.com/menu_items.json");
 
     NarrowItDownController.$inject = ['MenuSearchService'];
     MenuSearchService.$inject = ['$q', '$http', 'ApiUrl'];
 
+
+    function FoundItemsDirective() {
+        var ddo = {
+            templateUrl: 'foundItems.html',
+            scope: {
+                items: '<',
+                myTitle: '@title',
+                onRemove: '&'
+            },
+            controller: NarrowItDownController,
+            controllerAs: 'list',
+            bindToController: true
+        };
+
+        return ddo;
+    }
+
     function NarrowItDownController(MenuSearchService) {
         var controller = this;
 
+        controller.title = "found items";
         controller.searchTerm = "";
+        controller.found = [];
 
         controller.getMatchedMenuItems = function () {
             var promise = MenuSearchService.getMatchedMenuItems(controller.searchTerm);
@@ -24,21 +44,33 @@
                 console.log("Something went terribly wrong.", error);
             });
         };
+
+        controller.removeItem = function (index) {
+            console.log("remove", index);
+            controller.found.splice(index, 1);
+        };
+
+        controller.listIsEmpty = function () {
+            return controller.found.length === 0;
+        };
     }
 
     function MenuSearchService($q, $http, ApiUrl) {
         var service = this;
 
-        service.getMatchedMenuItems = function (searchItem) {
+        service.getMatchedMenuItems = function (searchTerm) {
             var deferred = $q.defer();
-            var term = searchItem.toLowerCase();
+            var term = searchTerm.toLowerCase();
             getMenuItemsPromise().then(function (response) {
                 var res = response.data.menu_items
                     .filter(function (item) {
                         return item.description.toLowerCase().includes(term);
                     })
                     .map(function (item) {
-                        return {short_name: item.short_name, name: item.name, description: item.description};
+                        return {
+                            short_name: item.short_name,
+                            name: item.name,
+                            description: item.description};
                     });
 
                 deferred.resolve(res);
