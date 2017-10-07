ddcd1e39b4fd856aec3f74cb862fb0dfd3f114d9
Alexander Karpov <alexander.karpoff@gmail.com>
2017-10-07 10:02:50 +0300

    Module 4 assignment: initial state

--- docs/ang-js/module4/src/menuapp.module.js
+++ docs/ang-js/module4/src/menuapp.module.js
@@ -1,30 +1,6 @@
 (function () {
-
     'use strict';
 
     angular.module('MenuApp', ['ui.router']);
-
-    angular.module('MenuApp').config(RoutesConfig);
-
-    RoutesConfig.$inject = ['$stateProvider', '$urlRouterProvider'];
-
-    function RoutesConfig($stateProvider, $urlRouterProvider) {
-
-        // Redirect to tab 1 if no other URL matches
-        $urlRouterProvider.otherwise('/tab1');
-
-        // Set up UI states
-        $stateProvider
-            .state('categories', {
-                url: '/categories',
-                templateUrl: 'src/templates/categories.html'
-            })
-
-            .state('items', {
-                url: '/items',
-                templateUrl: 'src/templates/items.html'
-            });
-    }
-
 
 })();
--- docs/ang-js/module4/src/routes.js
+++ docs/ang-js/module4/src/routes.js
@@ -1,3 +1,41 @@
 (function () {
     'use strict';
+
+    angular.module('MenuApp')
+        .config(RoutesConfig);
+
+    RoutesConfig.$inject = ['$stateProvider', '$urlRouterProvider'];
+
+    function RoutesConfig($stateProvider, $urlRouterProvider) {
+
+        // Redirect to home page if no other URL matches
+        $urlRouterProvider.otherwise('/');
+
+        // *** Set up UI states ***
+        $stateProvider
+
+            .state('home', {
+                url: '/',
+                templateUrl: 'src/templates/home.template.html'
+            })
+
+            .state('categoriesList', {
+                url: '/categories-list',
+                templateUrl: 'src/templates/categories.template.html',
+                controller: 'MainShoppingListController as mainList',
+                resolve: {
+                    items: ['MenuDataService', function (MenuDataService) {
+                        return MenuDataService.getAllCategories();
+                    }]
+                }
+            })
+
+            .state('categoriesList.items', {
+                url: '/categories-list/{categoryShortName}',
+                templateUrl: 'src/templates/items.template.html'
+                // controller: "ItemDetailController as itemDetail"
+            });
+
+    }
+
 })();
