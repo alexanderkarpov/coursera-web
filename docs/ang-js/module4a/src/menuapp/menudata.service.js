b7ea70d0a95b6ca6d33257a65aa1e68528e2a5a4
Alexander Karpov <alexander.karpoff@gmail.com>
2017-10-11 22:32:57 +0300

    Module 4 assignment

--- docs/ang-js/module4a/src/menuapp/menudata.service.js
+++ docs/ang-js/module4a/src/menuapp/menudata.service.js
@@ -5,27 +5,16 @@
         .service('MenuDataService', MenuDataService);
 
 
-    MenuDataService.$inject = ['$q'];
+    MenuDataService.$inject = ['$q', '$http'];
 
-    function MenuDataService($q) {
+    function MenuDataService($q, $http) {
         var service = this;
 
-        var items = [];
-
-        items.push({
-            id: 81,
-            short_name: "L",
-            name: "Lunch",
-            special_instructions: "Sunday-Friday 11:15am-3:00pm. Served with your choice of rice " +
-            "(Vegetable Fried Rice, Steamed Rice, Brown Rice), AND EITHER soup (Hot \u0026 Sour," +
-            " Wonton, Vegetable, Egg Drop, Chicken Corn Soup) OR veggie egg roll. $1.00 extra to " +
-            "have both soup and egg roll.",
-            url:"https://davids-restaurant.herokuapp.com/categories/81.json"
-        });
         service.getAllCategories = function () {
-            var deferred = $q.defer();
-            deferred.resolve(items);
-            return deferred.promise;
+            return $http({
+                method: "GET",
+                url: ("https://davids-restaurant.herokuapp.com/categories.json")
+            });
         };
 
         service.getItemsForCategory = function (categoryShortName) {
@@ -34,7 +23,6 @@
             return deferred.promise;
         };
     }
-
 
 
 })();
