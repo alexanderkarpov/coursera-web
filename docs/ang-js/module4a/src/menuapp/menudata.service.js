c2b01f14e4b66456e774dd368ba9ef87232d4af9
Alexander Karpov <alexander.karpoff@gmail.com>
2017-10-11 22:12:25 +0300

    Module 4 assignment

--- docs/ang-js/module4a/src/menuapp/menudata.service.js
+++ docs/ang-js/module4a/src/menuapp/menudata.service.js
@@ -10,26 +10,18 @@
     function MenuDataService($q) {
         var service = this;
 
-        // List of shopping items
         var items = [];
 
-        // Pre-populate a no cookie list
         items.push({
-            name: "Sugar",
-            quantity: "2 bags",
-            description: "Sugar used for baking delicious umm... baked goods."
+            id: 81,
+            short_name: "L",
+            name: "Lunch",
+            special_instructions: "Sunday-Friday 11:15am-3:00pm. Served with your choice of rice " +
+            "(Vegetable Fried Rice, Steamed Rice, Brown Rice), AND EITHER soup (Hot \u0026 Sour," +
+            " Wonton, Vegetable, Egg Drop, Chicken Corn Soup) OR veggie egg roll. $1.00 extra to " +
+            "have both soup and egg roll.",
+            url:"https://davids-restaurant.herokuapp.com/categories/81.json"
         });
-        items.push({
-            name: "flour",
-            quantity: "1 bags",
-            description: "High quality wheat flour. Mix it with water, sugar, 2 raw eggs."
-        });
-        items.push({
-            name: "Chocolate Chips",
-            quantity: "3 bags",
-            description: "Put these in the dough. No reason, really. Gotta store them somewhere!"
-        });
-
         service.getAllCategories = function () {
             var deferred = $q.defer();
             deferred.resolve(items);
