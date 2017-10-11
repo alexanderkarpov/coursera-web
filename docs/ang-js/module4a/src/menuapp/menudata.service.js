5ae7c7220f9b9016aac952822c9cd1c6c481e4f9
Alexander Karpov <alexander.karpoff@gmail.com>
2017-10-11 23:39:19 +0300

    Module 4 assignment

--- docs/ang-js/module4a/src/menuapp/menudata.service.js
+++ docs/ang-js/module4a/src/menuapp/menudata.service.js
@@ -11,10 +11,17 @@
         var service = this;
 
         service.getAllCategories = function () {
-            return $http({
+            var deferred = $q.defer();
+            var promise = $http({
                 method: "GET",
                 url: ("https://davids-restaurant.herokuapp.com/categories.json")
             });
+            promise.then(function (response) {
+                deferred.resolve(response.data)
+            }).catch(function (error) {
+                console.log("Something went terribly wrong.", error);
+            });
+            return deferred.promise;
         };
 
         service.getItemsForCategory = function (categoryShortName) {
